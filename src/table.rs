705d2bbf3ec7f114768e0d6a01d30b688fef01af
TIMMYLOPK <timmy.12345wer11@gmail.com>
2023-08-16 17:39:16 +0800

    feat: better perf

--- src/table.rs
+++ src/table.rs
@@ -2,9 +2,7 @@ use futures::{stream::iter, StreamExt};
 use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use owo_colors::OwoColorize;
 use reqwest::{Client, StatusCode};
-use std::sync::Arc;
 use tabled::{builder::Builder, settings::Style};
-use tokio::sync::Semaphore;
 use version_compare::{Cmp, Version};
 
 const NPM_URL: &str = "https://registry.npmjs.org/";
@@ -23,95 +21,69 @@ pub async fn get_report_table(
     let spinner_style = ProgressStyle::with_template("{prefix:.bold.dim} {spinner} {wide_msg}")
         .unwrap()
         .tick_chars("⠁⠂⠄⡀⢀⠠⠐⠈ ");
-    pb.clone().with_style(spinner_style.clone());
+    pb.set_style(spinner_style);
     pb.set_prefix(format!("{}", "[3/3]".bold().dimmed()));
 
     let mut outdated_deps = Vec::new();
 
     let max_concurrent = num_cpus::get();
-    let sem = Arc::new(Semaphore::new(max_concurrent));
-    let stream = iter(
-        deps_list
-            .clone()
-            .into_iter()
-            .map(|(name, version, _)| async {
-                pb.set_message(format!("Checking {}...", name));
-                let permit = sem.acquire().await.unwrap();
-                let latest_version = get_package(&name, &client).await?;
-                let current_version = Version::from(&version).unwrap();
-                let latest_version = Version::from(&latest_version).unwrap();
-                let status = match current_version.compare(&latest_version) {
-                    Cmp::Lt => "Outdated",
-                    Cmp::Eq => "Up to date",
-                    _ => panic!("Unknown status"),
-                };
+    let mut stream = iter(deps_list.iter().cloned())
+        .map(|x| {
+            let client = client.clone();
+            let pb = pb.clone();
+            tokio::spawn(async move {
+                pb.set_message(format!("{} {}", "Checking".dimmed(), x.0));
+                let name = x.0;
+                let current_version = x.1;
+                let url = format!("{}{}", NPM_URL, name);
+                let resp = client.get(&url).send().await.unwrap();
+                let status = resp.status();
 
-                pb.inc(1);
-
-                drop(permit);
-
-                Ok::<_, Box<dyn std::error::Error>>((
-                    name,
-                    version,
-                    latest_version.to_string(),
-                    status.to_string(),
-                ))
-            }),
-    )
-    .buffer_unordered(max_concurrent)
-    .collect::<Vec<_>>();
-
-    pb.finish_with_message("waiting...");
-
-    let stream = stream.await;
-
-    for x in stream {
-        match x {
-            Ok(x) => {
-                match x.3.as_str() {
-                    "Outdated" => {
-                        outdated_deps.push(format!(
-                            "{}{}{} {} {}{}",
-                            x.0.yellow(),
-                            "(".dimmed(),
-                            x.1.dimmed(),
-                            "->".dimmed(),
-                            x.2.dimmed(),
-                            ")".dimmed()
-                        ));
+                match status {
+                    StatusCode::OK => {
+                        let resp = resp.json::<serde_json::Value>().await.unwrap();
+                        let latest_version =
+                            resp["dist-tags"]["latest"].as_str().unwrap().to_string();
+                        (name, current_version, latest_version)
                     }
-                    _ => {}
+                    StatusCode::NOT_FOUND => (name, current_version, "Not found".to_string()),
+                    _ => (name, current_version, "Unknown".to_string()),
                 }
-                builder.push_record(vec![x.0, x.1, x.2, colorized_status(&x.3)]);
+            })
+        })
+        .buffer_unordered(max_concurrent);
+
+    while let Some(x) = stream.next().await {
+        let x = x.unwrap();
+        let current_version = Version::from(&x.1).unwrap();
+        let latest_version = Version::from(&x.2).unwrap();
+        let status = match current_version.compare(&latest_version) {
+            Cmp::Lt => "Outdated",
+            Cmp::Eq => "Up to date",
+            _ => panic!("Unknown status"),
+        };
+
+        match status {
+            "Outdated" => {
+                outdated_deps.push(format!(
+                    "{}{}{} {} {}{}",
+                    x.0.yellow(),
+                    "(".dimmed(),
+                    x.1.dimmed(),
+                    "->".dimmed(),
+                    x.2.dimmed(),
+                    ")".dimmed()
+                ));
             }
-            Err(e) => {
-                if e.downcast_ref::<reqwest::Error>()
-                    .map_or(false, |e| e.status() == Some(StatusCode::NOT_FOUND))
-                {
-                    pb.inc(1);
-                    continue;
-                }
-                panic!("Error: {}", e);
-            }
+            _ => {}
         }
+
+        builder.push_record(vec![x.0, x.1, x.2, colorized_status(&status)]);
     }
 
     let table = builder.build().with(Style::modern()).to_string();
 
     Ok((table, outdated_deps))
-}
-
-async fn get_package(name: &str, client: &Client) -> Result<String, Box<dyn std::error::Error>> {
-    let url = format!("{}{}", NPM_URL, name);
-    let resp = client
-        .get(&url)
-        .send()
-        .await?
-        .json::<serde_json::Value>()
-        .await?;
-    let latest_version = resp["dist-tags"]["latest"].to_string();
-    let latest_version = latest_version.replace("\"", "");
-    Ok(latest_version)
 }
 
 fn colorized_status(status: &str) -> String {
